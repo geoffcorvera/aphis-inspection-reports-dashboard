53b858859a8568f6f1d6aecddb0f3bc7fa0f2b1f
Rowan Copley <ananke@rowan.earth>
2024-06-27 16:56:10 -0700

    WIP: geocode addresses and then somehow add back to db

---
+++ scripts/geocode.py
@@ -0,0 +1,39 @@
+from dotenv import load_dotenv
+import json
+import requests
+import os
+from sqlite_utils import Database
+
+load_dotenv()
+
+def _geocode(address):
+    url = f"https://maps.googleapis.com/maps/api/geocode/json?address={address}&key={os.environ['GOOGLE_MAPS_API_KEY']}"
+    print(url)
+    r = requests.get(url)
+    j = r.json()['results'][0]
+    print(address)
+    print(json.dumps(j, indent=2))
+    return j['geometry']['location']['lat'], j['geometry']['location']['lng']
+
+addy_dict = {}
+def geocode(address):
+    if address in addy_dict:
+        return addy_dict[address]
+    addy_dict[address] = _geocode(address)
+    return addy_dict[address]
+
+geocode("14330 W. Sylvanfield Dr. Houston, TX 77014")
+
+db = Database('aphis_reports.db', recreate=False)
+for row in db.query('select rowid, pdf_customer_addr, customer_state from inspections limit 10'):
+    print(row)
+    lat,lng = geocode(row['pdf_customer_addr'])
+    try:
+        with open('geocode.json') as f:
+            j = json.loads(f.read())
+    except:
+        j = []
+    j.append({"lat": lat, "lng": lng, **row})
+    with open('_geocode.json', 'w') as f:
+        f.write(json.dumps(j))
+    os.rename('_geocode.json', 'geocode.json')
