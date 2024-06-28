bd253a226d80f0aea8655820c2e2afb0e00de55e
Geoff Corvera <geoffcorvera@gmail.com>
2024-06-27 19:37:36 -0700

    drop table on prejoin

--- Makefile
+++ Makefile
@@ -14,13 +14,6 @@ fetch-data:
 	wget -P aphis-inspection-reports/data/combined https://raw.githubusercontent.com/data-liberation-project/aphis-inspection-reports/main/data/combined/inspections-citations.csv
 	wget -P aphis-inspection-reports/data/combined https://github.com/data-liberation-project/aphis-inspection-reports/raw/main/data/combined/inspections.csv
 
-prejoin-data:
-	.venv/bin/sqlite-utils drop-table aphis_reports.db citation_inspection
-	.venv/bin/sqlite-utils aphis_reports.db "select narrative, desc, web_siteName, kind, hash_id, web_inspectionDate, code, repeat, pdf_insp_type, pdf_animals_total, web_certType, pdf_customer_id, pdf_customer_name, pdf_customer_addr, customer_state, pdf_site_id from citations natural join inspections" --csv > prejoined.csv
-	.venv/bin/sqlite-utils insert aphis_reports.db citation_inspection prejoined.csv --csv
-	.venv/bin/sqlite-utils transform aphis_reports.db citation_inspection --add-foreign-key hash_id inspections hash_id
-	rm prejoined.csv
-
 create-db:
 	.venv/bin/python3 scripts/create_db.py
 	.venv/bin/sqlite-utils enable-fts aphis_reports.db citations narrative
@@ -32,7 +25,7 @@ load-embeddings:
 	sqlite-utils rename-table aphis_reports.db _embeddings_citations _embeddings_citation_inspection
 	sqlite-utils transform aphis_reports.db citation_inspection --pk rowid
 	
-database: fetch-data create-db prejoin-data load-embeddings
+database: fetch-data create-db load-embeddings
 
 serve:
 	.venv/bin/datasette ./aphis_reports.db --plugins-dir=plugins/ --metadata metadata.json --setting sql_time_limit_ms 5000 --template-dir=templates/
--- scripts/create_db.py
+++ scripts/create_db.py
@@ -2,18 +2,21 @@ from sqlite_utils import Database
 import pandas as pd
 
 def read_csv_file(filepath, pk=None):
-    df = pd.read_csv(filepath)
-    df.drop_duplicates(subset=pk, inplace=True)
+    df = pd.read_csv(filepath).drop_duplicates(subset=pk)
     return df.to_dict('records')
-    
-def create_db():
-    db = Database('aphis_reports.db', recreate=True)
 
-    inspection_rows = read_csv_file('aphis-inspection-reports/data/combined/inspections.csv', pk=['hash_id'])
-    db['inspections'].insert_all(inspection_rows, pk='hash_id')
+join_query = "select narrative, desc, web_siteName, kind, hash_id, web_inspectionDate, code, repeat, pdf_insp_type, pdf_animals_total, web_certType, pdf_customer_id, pdf_customer_name, pdf_customer_addr, customer_state, pdf_site_id from citations natural join inspections"
 
-    citations_rows = read_csv_file('aphis-inspection-reports/data/combined/inspections-citations.csv')
-    db['citations'].insert_all(citations_rows, foreign_keys=[('hash_id', 'inspections', 'hash_id')])
+def prejoin_data():
+    db = Database('aphis_reports.db')
+    db["citation_inspection"].drop(ignore=True)
+    db["citation_inspection"].insert_all(db.query(join_query), foreign_keys=[('hash_id', 'inspections', 'hash_id')])
 
-if __name__ == '__main__':
-    create_db()
+db = Database('aphis_reports.db', recreate=True)
+
+inspection_rows = read_csv_file('aphis-inspection-reports/data/combined/inspections.csv', pk=['hash_id'])
+db['inspections'].insert_all(inspection_rows, pk='hash_id')
+citations_rows = read_csv_file('aphis-inspection-reports/data/combined/inspections-citations.csv')
+db['citations'].insert_all(citations_rows, foreign_keys=[('hash_id', 'inspections', 'hash_id')])
+
+prejoin_data()
