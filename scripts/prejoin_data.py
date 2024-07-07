619c4bcf5bdc2961746ac0962d7726d2267da5c4
Rowan Copley <ananke@rowan.earth>
2024-07-07 12:22:21 -0700

    put doccloud link into joined table and surface it; construct link to inspection from hash_id

--- scripts/prejoin_data.py
+++ scripts/prejoin_data.py
@@ -1,7 +1,7 @@
 import os
 from sqlite_utils import Database
 
-join_query = "select narrative, desc, web_siteName, kind, hash_id, web_inspectionDate, code, repeat, pdf_insp_type, pdf_animals_total, web_certType, pdf_customer_id, pdf_customer_name, pdf_customer_addr, customer_state, pdf_site_id from citations natural join inspections"
+join_query = "select narrative, desc, web_siteName, kind, hash_id, web_inspectionDate, code, repeat, pdf_insp_type, pdf_animals_total, web_certType, pdf_customer_id, pdf_customer_name, pdf_customer_addr, customer_state, pdf_site_id, doccloud_url from citations natural join inspections"
 
 def prejoin_data():
     db = Database('aphis_reports.db')
